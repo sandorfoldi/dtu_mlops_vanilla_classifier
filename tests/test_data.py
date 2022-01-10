fff2a7ce305a1a01743862ca205be865b807c8d5
sandorfoldi <sandorfoldi98@gmail.com>
2022-01-10 18:42:54 +0100

    add skipif to test_data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,14 @@
 import torch
+import pytest
 
 
-tensor_x = torch.load('data/processed/train_imgs_tensor.pt')
-tensor_y = torch.load('data/processed/train_labels_tensor.pt')
+data_path = 'data/processed/train_imgs_tensor.pt'
 
-assert tensor_x.shape == torch.Size([40000, 784]), 'train_img has wrong shape:('
-assert tensor_y.shape == torch.Size([40000]), 'train_labels has wrong shape:('
+@pytest.mark.skipif(not os.path.exists(data_path), reason="Data files not found")
+def test_data_processed_shape():    
+    tensor_x = torch.load('data/processed/train_imgs_tensor.pt')
+    tensor_y = torch.load('data/processed/train_labels_tensor.pt')
+
+    assert tensor_x.shape == torch.Size([40000, 784]), 'train_img has wrong shape:('
+    assert tensor_y.shape == torch.Size([40000]), 'train_labels has wrong shape:('
 
